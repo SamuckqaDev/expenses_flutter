9c39c84dbfb478128328003e78913d9b4d758ab2
samuckqaDev <samuckqa@hotmail.com>
2025-04-02 16:30:38 -0300

    List expenses

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:expenses/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:uuid/uuid.dart';
 
 main() => runApp(const ExpensesApp());
 
@@ -7,21 +9,36 @@ class ExpensesApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Homepage(),
     );
   }
 }
 
 class Homepage extends StatelessWidget {
-  const Homepage({super.key});
+  final _transactions = [
+    Transaction(
+      uuid: const Uuid().v4(),
+      title: "new shoes",
+      value: 69.99,
+      date: DateTime.now(),
+    ),
+    Transaction(
+      uuid: const Uuid().v4(),
+      title: "Smartphone",
+      value: 999.99,
+      date: DateTime.now(),
+    ),
+  ];
+
+  Homepage({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text(
-          'Expenses Tracker',
+          'Personal Expenses',
           style: TextStyle(
             fontSize: 20,
             fontWeight: FontWeight.bold,
@@ -30,8 +47,41 @@ class Homepage extends StatelessWidget {
         ),
         backgroundColor: Colors.purple,
       ),
-      body: const Center(
-        child: Text('Hello World'),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: <Widget>[
+          const SizedBox(
+            child: Card(
+              color: Colors.purple,
+              elevation: 5,
+              child: Text(
+                "gauge",
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+          ),
+          Column(children: [
+            ...(_transactions.map(
+              (tr) => Card(
+                child: Row(
+                  children: [
+                    Text(
+                      tr.value.toString(),
+                    ),
+                    Column(
+                      children: [
+                        Text(tr.title),
+                        Text(tr.date.toString())
+                      ],
+                    )
+                  ],
+                )
+
+              ),
+            )),
+          ])
+        ],
       ),
     );
   }
